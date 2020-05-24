73fde9c84d6c3d7e03c73416809ecde9bfd60e32
ikcilrep <frajerzycki@protonmail.com>
2020-05-24 21:19:04 +0200

    Add navbar

---
+++ src/components/Joke.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react"
+import { CircularProgress } from '@material-ui/core';
+
+export default class Joke extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: {},
+            loading: true
+        };
+    }
+    render() {
+        const { data, loading } = this.state;
+        if (loading) {
+            return <div><CircularProgress /></div>;
+        }
+
+        return (
+            <div>
+                <img src={data.icon_url}></img>
+                <h1>
+                    {data.value}
+                </h1>
+            </div>
+        );
+    }
+
+    async componentDidMount() {
+        const data = await fetch("https://api.chucknorris.io/jokes/random");
+        const dataJSON = await data.json();
+
+        if (dataJSON) {
+            this.setState(
+                { data: dataJSON, loading: false }
+            )
+        }
+    }
+}
---
+++ src/components/NavBar.js
@@ -0,0 +1,30 @@
+import { Toolbar, IconButton, Typography, AppBar, Button } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
+
+export default function NavBar(props) {
+    const classes = useStyles();
+    return (<AppBar position="static">
+        <Toolbar>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+                Chuck Norris jokes
+      </Typography>
+        </Toolbar>
+    </AppBar>);
+}
